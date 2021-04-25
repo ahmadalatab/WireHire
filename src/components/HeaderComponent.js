5e0f777f18e77a40ce3c8abc7359cd9423282a80
Ahmad Alatab <ahmadatab86@gmail.com>
2021-04-25 00:01:47 -0700

    created login modal and created empty profile component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Button, Form} from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -7,10 +7,13 @@ class Header extends Component {
     constructor(props) {
         super(props);
 
-        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
     }
 
     toggleNav() {
@@ -19,36 +22,63 @@ class Header extends Component {
         });
     }
 
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
     render() {
         return (
-            <Navbar sticky="top" expand="md">
-                <div className="container">
-                    <NavbarBrand className="mr-auto" href="/">WireHire</NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNav} />
-                    <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar className="mr-auto">
-                            <NavItem className="nav-item">
-                                <NavLink className="nav-link" to="/home">
-                                    <i className="fa fa-home fa-lg" /> Home
+            <React.Fragment>
+                <Navbar sticky="top" expand="md">
+                    <div className="container">
+                        <NavbarBrand className="mr-auto" href="/">WireHire</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar className="mr-auto">
+                                <NavItem className="nav-item">
+                                    <NavLink className="nav-link" to="/home">
+                                        <i className="fa fa-home fa-lg" /> Home
+                                        </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/profile">
+                                        Profile
                                     </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/profile">
-                                    Profile
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/jobs">
-                                    Jobs
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                        <Form inline>                         
-                            <Button className="float-sm-right" variant="outline-success">Login</Button>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/jobs">
+                                        Jobs
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <span>                         
+                                <Button className="navbar-text float-sm-right" variant="outline-success" onClick={this.toggleModal}>
+                                    <i className="fa fa-sign-in fa-lg" />Login
+                                </Button>
+                            </span>
+                        </Collapse>
+                    </div>
+                </Navbar>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal} >Login</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" placeholder="username" innerRef={input => this.username = input}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" placeholder="password" innerRef={input => this.password = input} />
+                            </FormGroup>
+                                <Button className="modalLogin" type="submit" value="submit" color="info">Login</Button>
+                                <p class="message">Not registered? <a href="#">Create an account</a></p>
                         </Form>
-                    </Collapse>
-                </div>
-            </Navbar>
+                    </ModalBody>
+                </Modal>             
+            </React.Fragment>
         );
     }
 }
