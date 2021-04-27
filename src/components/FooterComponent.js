584b7652e0efac6da368efd911253ad9129057e0
fidwu <fidellapwu@gmail.com>
2021-04-26 21:23:07 -0700

    fix footer formatting

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Footer(props) {
+function Footer() {
     return (
         <footer id="footer">
             <div className="container">
-                <div className="row">
-                    <div className="col-4 col-sm-2 offset-1 footer-items">
+                <div className="row justify-content-center">
+                    <div className="col-md-4 col-sm-6 footer-items">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to="/">Home</Link></li>
@@ -14,13 +14,13 @@ function Footer(props) {
                             <li><Link to="/jobs">Jobs</Link></li>
                         </ul>
                     </div>
-                    <div className="col-6 col-sm-3 text-center">
+                    <div className="col-md-4 col-sm-6 text-center">
                         <h5 className="footer-items">Social</h5>
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram footer-items" /></a>{' '}
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter footer-items" /></a>{' '}
                         <a className="btn btn-social-icon btn-google" href="http://linkedin.com/"><i className="fa fa-linkedin footer-items" /></a>
                     </div>
-                    <div className="col-sm-4 text-center">
+                    <div className="col-md-4 col-sm-6 text-center">
                         <a role="button" className="btn btn-link" href="tel:+194912345678"><i className="fa fa-phone footer-items" /> 1-949-1234-5678</a><br />
                         <a role="button" className="btn btn-link " href="mailto:contact@wirehire.com"><i className="fa fa-envelope-o footer-items" /> contact@wirehire.com</a>
                     </div>
